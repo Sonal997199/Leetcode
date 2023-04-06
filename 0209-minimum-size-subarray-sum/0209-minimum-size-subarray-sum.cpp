baae25d0d35357238a246ee0e6e540af2797b7dc
Sonal Gupta <sonalgup947@gmail.com>
2023-04-06 20:40:42 +0530

    Time: 63 ms (6.97%), Space: 29.1 MB (7.65%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
-        int i=0,j=0,sum=0;
+        /*int i=0,j=0,sum=0;
         int mn=INT_MAX;
         bool flag=0;
         int n=nums.size();
@@ -24,6 +24,26 @@ public:
             j++;
         }
         if(!flag)return 0;
-        return mn;
+        return mn;*/
+        
+        
+        
+        
+        
+        //Using Binary Search and Prefix sum
+        int n=nums.size();
+        vector<int> v(n+1,0);
+        for(int i=0;i<n;i++)v[i+1]=v[i]+nums[i];
+        
+        long long ans=INT_MAX;
+        for(int i=0;i<n;i++)
+        {
+            long long sum=target+v[i];
+            long long idx=lower_bound(v.begin(),v.end(),sum)-v.begin();
+            if(idx!=n+1)ans=min(ans,idx-i);
+        }
+        
+        if(ans==INT_MAX)return 0;
+        return ans;
     }
 };
