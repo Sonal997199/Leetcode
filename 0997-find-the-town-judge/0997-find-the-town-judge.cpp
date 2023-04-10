0e8d207e2d36e88a476c17c0cab63999d4a21cda
Sonal Gupta <sonalgup947@gmail.com>
2023-04-10 16:44:29 +0530

    Time: 183 ms (70.64%), Space: 60.8 MB (73.07%) - LeetHub

---
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        vector<pair<int,int>>mp(n+1,{0,0});
+        for(int i=0;i<trust.size();i++){
+            mp[trust[i][0]].first +=1;
+            mp[trust[i][1]].second +=1;
+        }
+
+        for(int i=1;i<=n;i++){
+            if(mp[i].first == 0 && mp[i].second == n-1){return i;}
+        }
+        
+        return -1;
+    }
+};
