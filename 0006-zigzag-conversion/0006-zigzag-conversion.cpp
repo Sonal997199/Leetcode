90cfe23d0445a590c65966966407664021978778
Sonal Gupta <sonalgup947@gmail.com>
2023-04-10 21:15:51 +0530

    Time: 3 ms (98.48%), Space: 8.1 MB (92.72%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        string res="";
+        if(numRows==1)
+            return s;
+        for(int i=0;i<numRows;i++)
+        {
+            bool flag=true;
+            int j=i;
+            //for(int j=i;j<s.length();)
+            while(j<s.size())
+            {
+                res+=s[j];
+                if(i==0 || i==numRows-1)
+                {
+                    j+=2*(numRows-1);
+                    continue;
+                }
+                j+=flag?(2*(numRows-i-1)):2*i;
+                flag=!flag;
+            }
+        }
+        return res;
+    }
+};
