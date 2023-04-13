37ec5dffc8bf57a23b1842311643cc38ab932e99
Sonal Gupta <sonalgup947@gmail.com>
2023-04-13 21:17:00 +0530

    Time: 281 ms (59.70%), Space: 94.9 MB (64.55%) - LeetHub

---
+++ 1898-maximum-number-of-removable-characters/1898-maximum-number-of-removable-characters.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    int maximumRemovals(string s, string p, vector<int>& removable) {
+        /*int l=0;
+        int h=removable.size()-1;
+        int ans=0;
+        while(l<=h)
+        {
+            int mid=l+(h-l)/2;
+            string s1=s;
+            for(int i=0;i<mid;i++)
+            {
+                s1[removable[i]]='A';
+            }
+            int t=p.size(),k=0;
+            for(int i=0;i<s1.size() && k<t;i++)
+            {
+                if(s1[i]==p[k])k++;
+            }
+            
+            if(t==k)
+            {
+                ans=mid;
+                l=mid+1;
+            }
+            else h=mid-1;
+        }
+        return ans;*/
+        
+        
+        
+        int i=0, j=removable.size(), ans=0;
+        while(i<=j){
+            int mid=i+(j-i)/2;
+            string s1=s;
+            for(int i=0; i<mid; i++){
+                s1[removable[i]]='A';
+            }
+            int l=p.size(), k=0;
+            for(int i=0; i<s1.size() && k<l; i++){
+                if(s1[i]==p[k]){k++;}
+            }
+            cout<<mid<<" "<<l<<" "<<k<<endl;
+            if(l==k){
+                ans=mid;
+                i=mid+1;
+            }
+            else{
+                j=mid-1;
+            }
+        }
+        return ans;
+    }
+};
