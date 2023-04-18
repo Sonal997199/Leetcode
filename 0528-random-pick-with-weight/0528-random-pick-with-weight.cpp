3873325cf337d80298eaeb4dd48f9c01cd986235
Sonal Gupta <sonalgup947@gmail.com>
2023-04-18 21:59:08 +0530

    Time: 78 ms (92.81%), Space: 40.3 MB (77.41%) - LeetHub

---
+++ 0528-random-pick-with-weight/0528-random-pick-with-weight.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> w;
+    Solution(vector<int>& w) {
+        for(int i=1;i<w.size();i++)
+            w[i]=w[i]+w[i-1];
+        this->w = w;
+    }
+    
+    int pickIndex() {
+        int h=w.back();
+        int index=rand()%h;
+        index++;
+        int l=lower_bound(w.begin(),w.end(),index)-w.begin();
+        return l;
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(w);
+ * int param_1 = obj->pickIndex();
+ */
