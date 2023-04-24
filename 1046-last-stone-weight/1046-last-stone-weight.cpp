497d082fabe7f7810ea3effed014f602465fe524
Sonal Gupta <sonalgup947@gmail.com>
2023-04-24 18:06:35 +0530

    Time: 2 ms (49.25%), Space: 7.6 MB (41.71%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        int n=stones.size();
+        priority_queue<int> pq;
+        for(int i=0;i<stones.size();i++)
+        {
+            pq.push(stones[i]);
+        }
+        while(pq.size()!=1)
+        {
+            int y=pq.top();
+            pq.pop();
+            int x=pq.top();
+            pq.pop();
+            pq.push(y-x);
+        }
+        return pq.top();
+    }
+};
